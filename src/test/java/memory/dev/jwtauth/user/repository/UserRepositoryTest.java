407dab0cfbd1d55f5bd8133c7691502fd0cf6c87
memoryDev <115910144+memoryDev@users.noreply.github.com>
2025-12-12 17:09:40 +0900

    [Test] 회원가입 테스트 추가
    - repositoryTest추가

--- src/test/java/memory/dev/jwtauth/user/repository/UserRepositoryTest.java
+++ src/test/java/memory/dev/jwtauth/user/repository/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package memory.dev.jwtauth.user.repository;
 import memory.dev.jwtauth.user.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
@@ -49,6 +50,32 @@ class UserRepositoryTest {
         Assertions.assertEquals(savedUser.getUserId(), selectUser.getUserId());
         Assertions.assertEquals(savedUser.getPassword(), selectUser.getPassword());
         Assertions.assertTrue(passwordEncoder.matches(rawPassword, selectUser.getPassword()));
+    }
+
+    @Test
+    void 회원가입() {
+        org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder passwordEncoder =
+                new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder();
+
+        String userId = "test2";
+        String password = "1234";
+        String userName = "테스트2";
+        String role = "ROLE_USER";
+
+        User user = User.builder()
+                .userId(userId)
+                .password(passwordEncoder.encode(password))
+                .userName(userName)
+                .role(role)
+                .build();
+
+        // when
+        User savedUser = userRepository.save(user);
+
+        // then
+        Assertions.assertEquals(userId, savedUser.getUserId());
+        Assertions.assertTrue(passwordEncoder.matches(password, savedUser.getPassword()));
+        Assertions.assertEquals(role, savedUser.getRole());
     }
 
 }
